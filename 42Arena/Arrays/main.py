a888fa508116b6214eaa22e4fea45e73fe8c0124
ShaxriyorDiyorugli <shaxriyordiyorugli@gmail.com>
2026-04-11 22:41:52 +0500

    ARRAYS 2 masala(movezerotoends) yechimi

--- 42Arena/Arrays/main.py
+++ 42Arena/Arrays/main.py
@@ -3,7 +3,6 @@
 1. Sizga butun sonlardan iborat array va targetSum beriladi.
 Arraydagi ikki turli son yig‘indisi targetSum'ga teng bo‘lsa,
 ularni o‘sish tartibida qaytaradigan funksiya yozing.
-"""
 def twoNumberSum(array, targetSum):
     # setga o'tgazish orqali qidirishni ancha oshiramiz!
     seen = set()
@@ -17,3 +16,25 @@ def twoNumberSum(array, targetSum):
 
     # Agar juftlik topilmasa
     return []
+"""
+
+"""
+2. Berilgan sonlar ro'yxatidagi nollarni ro'yxat orqasiga o'tkazing,
+ lekin boshqa elementlar ketma-ketligi buzilmasin.
+Imkon qadar kamroq amal bajaring.
+Qo'shimcha xotiradan foydalanmang - 
+amallarni ro'yxat ustida bajaring.
+"""
+
+def moveZeroes(nums: list) -> list:
+    non_zero_pos = 0
+
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[non_zero_pos] = nums[i]
+            non_zero_pos += 1
+
+    for i in range(non_zero_pos, len(nums)):
+        nums[i] = 0
+
+    return nums
