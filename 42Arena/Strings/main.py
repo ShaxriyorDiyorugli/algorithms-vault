109e1952a4709813f223d64b6998d246111f2fe6
ShaxriyorDiyorugli <shaxriyordiyorugli@gmail.com>
2026-04-11 23:03:43 +0500

    42 Strings 1 masala

--- 42Arena/Strings/main.py
+++ 42Arena/Strings/main.py
@@ -1,4 +1,20 @@
-#1masala qavslar
+"""
+1. Masala
+So'z qabul qilib, u so'z palindromligini tekshiradigan funksiya yozing.
+Palindrom bo‘lsa — true, aks holda false qiymatini qaytarsin.
+💡 Palindrom — bu oldindan ham, orqadan ham bir xil o‘qiladigan so'z.
+E’tibor bering: bitta belgidan iborat so'zlar ham palindrom hisoblanadi.
+"""
+def isPalindrome(string: str) -> bool:
+    if string == string[::-1]:
+      return True
+    else:
+      return False
+if __name__ == "__main__":
+  text = input("Enter a word: ")
+  print(isPalindrome(text))
+
+#4masala qavslar
 """def is_valid(matn:str)->bool:
     stek = []
     for belgi in matn:
